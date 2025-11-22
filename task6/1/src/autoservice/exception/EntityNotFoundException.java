139bce9735c5f44365cb43f38b1fa594c56a2d7d
gama1726 <gama17one@gmail.com>
2025-11-22 03:35:23 +0300

    feat: добавление иерархии исключений для обработки ошибок
    - AutoserviceException - базовое исключение для автосервиса
    - EntityNotFoundException - исключение для случаев, когда сущность не найдена
    - ImportExportException - исключение для ошибок импорта/экспорта
    - ValidationException - исключение для ошибок валидации данных

---
+++ task6/1/src/autoservice/exception/EntityNotFoundException.java
@@ -0,0 +1,11 @@
+package autoservice.exception;
+
+/**
+ * Исключение, выбрасываемое когда сущность не найдена.
+ */
+public class EntityNotFoundException extends AutoserviceException {
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
+
---
+++ task6/1/src/autoservice/exception/ValidationException.java
@@ -0,0 +1,11 @@
+package autoservice.exception;
+
+/**
+ * Исключение, выбрасываемое при ошибках валидации данных.
+ */
+public class ValidationException extends AutoserviceException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
+
