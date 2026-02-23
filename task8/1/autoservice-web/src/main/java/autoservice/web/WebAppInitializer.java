af35f542b86a93fc7f731747725d02b56ac4381f
gama1726 <gama17one@gmail.com>
2026-02-23 16:59:39 +0300

    feat(auth): зарегистрировать фильтр безопасности и эндпоинт логина

--- task8/1/autoservice-web/src/main/java/autoservice/web/WebAppInitializer.java
+++ task8/1/autoservice-web/src/main/java/autoservice/web/WebAppInitializer.java
@@ -1,14 +1,21 @@
 package autoservice.web;
 
+import java.util.EnumSet;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import autoservice.config.AppConfig;
 import autoservice.database.FlywayMigrator;
+import autoservice.web.security.SecurityConfig;
+import jakarta.servlet.DispatcherType;
 import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletException;
 
 /**
  * Регистрация DispatcherServlet и корневого контекста Spring (без web.xml).
+ * Регистрирует фильтр Spring Security (springSecurityFilterChain).
  */
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -16,11 +23,15 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     public void onStartup(ServletContext servletContext) throws ServletException {
         FlywayMigrator.run();
         super.onStartup(servletContext);
+        servletContext.addFilter(
+                AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME,
+                new DelegatingFilterProxy(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME))
+            .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.ASYNC), false, "/*");
     }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{AppConfig.class};
+        return new Class<?>[]{AppConfig.class, SecurityConfig.class};
     }
 
     @Override
